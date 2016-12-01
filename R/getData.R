4b71e7c27a461117cb6ba030ba5f623cbf54b5f1
Carsten Behring <carsten.behring@gmail.com>
2016-12-01 13:09:08 +0100

    Initial commit

---
+++ R/getData.R
@@ -0,0 +1,28 @@
+#' Downloads measures as data frame from altas API at
+#' http://atlas.ecdc.europa.eu/public/AtlasService/rest/GetMeasuresResultsExportFile
+#' @importFrom magrittr %>%
+#' @param healthTopicId The healthTopicId to query data for
+#' @export
+GetMeasuresResultsExportFile <- function(healthTopicId) {
+    readr::read_csv(paste0("http://atlas.ecdc.europa.eu/public/AtlasService/rest/GetMeasuresResultsExportFile?healthTopicId=",healthTopicId,
+                    "&datasetId=27&measurePopulation=&measureIds=&measureTypes=I,Q&timeCodes=&geoCodes=&geoLevel=2&timeUnit=Year")) %>%
+      dplyr::mutate(NumValue = ifelse(NumValue == "-",NA,NumValue),
+             NumValue = as.numeric(NumValue))
+}
+
+#' Requests all helath topics from API.
+#' Calls enpoint http://atlas.ecdc.europa.eu/public/AtlasService/rest/GetHealthTopics
+#' @export
+GetHealthTopics = function() {
+  httr::GET("http://atlas.ecdc.europa.eu/public/AtlasService/rest/GetHealthTopics") %>%
+    httr::content(as = "text") %>%
+  tidyjson::as.tbl_json() %>%
+    tidyjson::enter_object("HealthTopics") %>%
+    tidyjson::gather_array() %>%
+    tidyjson::spread_values(Label = tidyjson::jstring("Label"),
+                  Code = tidyjson::jstring("Code"),
+                  Id = tidyjson::jstring("Id")) %>%
+    dplyr::select(Id,Label,Code)
+
+
+}
